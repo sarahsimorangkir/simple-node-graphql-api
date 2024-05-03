932b559d9903dc58b3b651bf1105a502bb594be1
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2024-05-03 17:30:25 +0700

    init project

---
+++ index.mjs
@@ -0,0 +1,17 @@
+import 'colors';
+import { ApolloServer } from 'apollo-server';
+import {typeDefs} from './schema.mjs';
+import {resolvers} from './resolvers.mjs';
+
+const API_PORT = 4000;
+const server = ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen( {port :API_PORT || 4000}).then(({url})=>{
+    console.log(`server is ready at :` .green + `${url}`.yellow);
+    console.log('query at ' .magenta + 'https://studio.apollographql.com/dev'.yellow)
+}).catch(error =>{
+    console.log(error);
+})
